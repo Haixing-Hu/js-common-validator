eb7ef896260d251acfde1deb3cade66a9e7b4305
Haixing Hu <starfish.hu@gmail.com>
2025-06-09 15:30:39 +0800

    refactor: convert Validator from object literal to ES6 class

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -24,26 +24,164 @@ import validateUppercaseAlphaNumberField from './validate-uppercase-alphanumber-
 import validateUrlField from './validate-url-field';
 import validateUsernameField from './validate-username-field';
 
-const Validator = {
-  alphaNum: validateAlphaNumberField,
-  bool: validateBoolField,
-  date: validateDateField,
-  time: validateTimeField,
-  datetime: validateDateTimeField,
-  email: validateEmailField,
-  enum: validateEnumField,
-  number: validateNumberField,
-  id: validateIntegerField,
-  int: validateIntegerField,
-  mobile: validateMobileField,
-  money: validateNumberField,
-  password: validatePasswordField,
-  personName: validatePersonNameField,
-  phone: validatePhoneField,
-  timestamp: validateTimestampField,
-  upperAlphaNum: validateUppercaseAlphaNumberField,
-  username: validateUsernameField,
-  url: validateUrlField,
-};
+/**
+ * The validator class providing static validation methods.
+ * 
+ * @class
+ * @author Haixing Hu
+ */
+class Validator {
+  /**
+   * Validates whether a field value is a string composed of numbers and letters.
+   * 
+   * @static
+   * @see {@link validateAlphaNumberField}
+   */
+  static alphaNum = validateAlphaNumberField;
+
+  /**
+   * Validates whether a field value is a boolean value or string representation of a boolean.
+   * 
+   * @static
+   * @see {@link validateBoolField}
+   */
+  static bool = validateBoolField;
+
+  /**
+   * Validates whether a field value represents a valid local date.
+   * 
+   * @static
+   * @see {@link validateDateField}
+   */
+  static date = validateDateField;
+
+  /**
+   * Validates whether a field value represents a valid local time.
+   * 
+   * @static
+   * @see {@link validateTimeField}
+   */
+  static time = validateTimeField;
+
+  /**
+   * Validates whether a field value represents a valid local date time.
+   * 
+   * @static
+   * @see {@link validateDateTimeField}
+   */
+  static datetime = validateDateTimeField;
+  
+  /**
+   * Validates whether a field value is a valid email address.
+   * 
+   * @static
+   * @see {@link validateEmailField}
+   */
+  static email = validateEmailField;
+
+  /**
+   * Validates whether a field value is a valid enumeration type or string representation.
+   * 
+   * @static
+   * @see {@link validateEnumField}
+   */
+  static enum = validateEnumField;
+
+  /**
+   * Validates whether a field value is a number or string representation of a number.
+   * 
+   * @static
+   * @see {@link validateNumberField}
+   */
+  static number = validateNumberField;
+
+  /**
+   * Validates whether a field value is an integer or string representation of an integer.
+   * 
+   * @static
+   * @see {@link validateIntegerField}
+   */
+  static id = validateIntegerField;
+
+  /**
+   * Validates whether a field value is an integer or string representation of an integer.
+   * 
+   * @static
+   * @see {@link validateIntegerField}
+   */
+  static int = validateIntegerField;
+
+  /**
+   * Validates whether a field value is a valid mobile phone number.
+   * 
+   * @static
+   * @see {@link validateMobileField}
+   */
+  static mobile = validateMobileField;
+
+  /**
+   * Validates whether a field value is a number or string representation of a number.
+   * 
+   * @static
+   * @see {@link validateNumberField}
+   */
+  static money = validateNumberField;
+
+  /**
+   * Validates whether a field value is a valid password.
+   * 
+   * @static
+   * @see {@link validatePasswordField}
+   */
+  static password = validatePasswordField;
+
+  /**
+   * Validates whether a field value is a valid person name.
+   * 
+   * @static
+   * @see {@link validatePersonNameField}
+   */
+  static personName = validatePersonNameField;
+
+  /**
+   * Validates whether a field value is a valid phone number.
+   * 
+   * @static
+   * @see {@link validatePhoneField}
+   */
+  static phone = validatePhoneField;
+
+  /**
+   * Validates whether a field value is a valid timestamp.
+   * 
+   * @static
+   * @see {@link validateTimestampField}
+   */
+  static timestamp = validateTimestampField;
+
+  /**
+   * Validates whether a field value is a string composed of uppercase letters and numbers.
+   * 
+   * @static
+   * @see {@link validateUppercaseAlphaNumberField}
+   */
+  static upperAlphaNum = validateUppercaseAlphaNumberField;
+
+  /**
+   * Validates whether a field value is a valid username.
+   * 
+   * @static
+   * @see {@link validateUsernameField}
+   */
+  static username = validateUsernameField;
+
+  /**
+   * Validates whether a field value is a valid URL.
+   * 
+   * @static
+   * @see {@link validateUrlField}
+   */
+  static url = validateUrlField;
+}
 
 export default Validator;
